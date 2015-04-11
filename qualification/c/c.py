1ae9b375df33ccfa6126f666a7045a5852739fd6
Rongduan Zhu <rongduan.zhu@gmail.com>
2015-04-11 18:06:48 +1000

    solved q3 but too slow

--- qualification/c/c.py
+++ qualification/c/c.py
@@ -21,12 +21,12 @@ def solver(f):
     test_cases = int(f.readline())
     for i in xrange(0, test_cases):
         strlen, repeat = f.readline().split()
-        base_str = f.readline()
-        gain_point = calc_friends(int(max_shy), shy_str)
-        print_solution(i + 1, gain_point)
+        base_str = f.readline().rstrip()
+        solution = can_split(base_str * int(repeat))
+        print_solution(i + 1, solution)
 
-def print_solution(t, gain_point):
-    print 'Case #{}: {}'.format(t, gain_point)
+def print_solution(t, solution):
+    print 'Case #{}: {}'.format(t, solution)
 
 def can_split(repeated_str):
     if len(repeated_str) < 3:
@@ -35,7 +35,7 @@ def can_split(repeated_str):
         return 'YES' if repeated_str == 'ijk' else "NO"
     else:
         str_len = len(repeated_str)
-        for i in xrange(1, str_len - 1):
+        for i in xrange(1, str_len - 2):
             if get_x(repeated_str[:i], 'i'):
                 for j in xrange(i + 1, str_len - 1):
                     if get_x(repeated_str[i:j], 'j'):
@@ -44,11 +44,11 @@ def can_split(repeated_str):
         return "NO"
 
 def get_combined(substr):
-    if lookup[substr]:
+    if substr in lookup:
         return lookup[substr]
 
     subsubstr = get_combined(substr[:-1])
-    if subsubstr[0] == '-':
+    if len(subsubstr) and subsubstr[0] == '-':
         subsubstr_combined = lookup[subsubstr[1:] + substr[-1]]
         if subsubstr_combined[0] == '-':
             lookup[substr] = subsubstr_combined[1:]
@@ -63,7 +63,7 @@ def get_combined(substr):
 
 def get_x(substr, x):
     combined = get_combined(substr)
-    return substr == x
+    return combined == x
 
 if __name__ == '__main__':
     f_name = sys.argv[1]
