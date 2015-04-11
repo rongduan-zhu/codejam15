0af2ed911c6b551d0eb0b1bc9f23dc8bdbfa4a35
Rongduan Zhu <rongduan.zhu@gmail.com>
2015-04-11 19:00:16 +1000

    improved performance slightly

--- qualification/c/c.py
+++ qualification/c/c.py
@@ -3,6 +3,7 @@ import sys
 
 lookup = {
     '': '',
+    '-': '-',
     'i': 'i',
     'j': 'j',
     'k': 'k',
@@ -22,13 +23,13 @@ def solver(f):
     for i in xrange(0, test_cases):
         strlen, repeat = f.readline().split()
         base_str = f.readline().rstrip()
-        solution = can_split(base_str * int(repeat))
+        solution = can_split(base_str * int(repeat), int(strlen))
         print_solution(i + 1, solution)
 
 def print_solution(t, solution):
     print 'Case #{}: {}'.format(t, solution)
 
-def can_split(repeated_str):
+def can_split(repeated_str, strlen):
     if len(repeated_str) < 3:
         return 'NO'
     elif len(repeated_str) == 3:
@@ -36,10 +37,10 @@ def can_split(repeated_str):
     else:
         str_len = len(repeated_str)
         for i in xrange(1, str_len - 2):
-            if get_x(repeated_str[:i], 'i'):
+            if get_x(repeated_str[:i], 'i', strlen):
                 for j in xrange(i + 1, str_len - 1):
-                    if get_x(repeated_str[i:j], 'j'):
-                        if get_x(repeated_str[j:], 'k'):
+                    if get_x(repeated_str[i:j], 'j', strlen):
+                        if get_x(repeated_str[j:], 'k', strlen):
                             return "YES"
         return "NO"
 
@@ -61,9 +62,28 @@ def get_combined(substr):
         lookup[substr] = subsubstr_combined
         return lookup[substr]
 
-def get_x(substr, x):
-    combined = get_combined(substr)
+def get_x(substr, x, strlen):
+    multiple = len(substr) / strlen
+    remainder = len(substr) % strlen
+
+    combined = get_combined(substr[:strlen])
+
+    if len(combined) and combined[0] == '-' and (not (multiple & 1)):
+        combined = combined[1:]
+
+    if multiple and remainder:
+        remainder_combined = get_combined(substr[:remainder])
+        combined = get_combined(add_str(combined, remainder_combined))
+
     return combined == x
+
+def add_str(str1, str2):
+    if len(str1) and len(str2) and str1[0] == '-' and str2[0] == '-':
+        return str1[1:] + str2[1:]
+    elif len(str2) and str2[0] == '-':
+        return '-' + str1 + str2[1:]
+    return str1 + str2
+
 
 if __name__ == '__main__':
     f_name = sys.argv[1]
